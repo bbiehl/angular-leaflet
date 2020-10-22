51fc5ec6566c3a4c8bd02c4191a0dcb40348b32e
Bradley Biehl <bradley.biehl@gmail.com>
2020-10-22 18:28:09 -0500

    map-install: map functional

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
+  map: L.Map;
+
   constructor() { }
 
   ngOnInit(): void {
-  }
+    const map = L.map('map').setView([41, -99], 6);
 
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      minZoom: 6,
+      maxZoom: 17,
+      tileSize: 512,
+      zoomOffset: -1,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+  }
 }
